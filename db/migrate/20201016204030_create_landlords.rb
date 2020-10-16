63c7378c3b4477fad1b20a2637e149e7b8e7ab24
d-otis <dan@dan-foley.com>
2020-10-16 16:50:08 -0400

    add Landlord model to app

---
+++ db/migrate/20201016204030_create_landlords.rb
@@ -0,0 +1,10 @@
+class CreateLandlords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :landlords do |t|
+      t.string :name
+      t.float :rating
+
+      t.timestamps
+    end
+  end
+end
