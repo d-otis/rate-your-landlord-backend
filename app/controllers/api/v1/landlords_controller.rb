ecfd48929fdabd4ebed17a79c233b0c210b62eef
d-otis <dan@dan-foley.com>
2020-10-17 19:26:19 -0400

    employ options to include properties in landlords#index serializer

--- app/controllers/api/v1/landlords_controller.rb
+++ app/controllers/api/v1/landlords_controller.rb
@@ -2,15 +2,15 @@ class Api::V1::LandlordsController < ApplicationController
   def index
     landlords = Landlord.order(created_at: :desc)
 
-    render json: LandlordSerializer.new(landlords).serialized_json
+    render json: LandlordSerializer.new(landlords, options).serialized_json
   end
 
   private
 
 # add in once properties serializer is fleshed out
-  # def options
-  #   options = {
-  #     include: [:properties]
-  #   }
-  # end
+  def options
+    options = {
+      include: [:properties]
+    }
+  end
 end
