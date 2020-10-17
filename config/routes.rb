10c436db4c3a00f59a049574bbb95d5f1e8dffff
d-otis <dan@dan-foley.com>
2020-10-17 17:30:29 -0400

    add properties#index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :landlords, only: [:index]
+      resources :properties, only: [:index]
     end
   end
 
