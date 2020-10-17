f713e86dcc6b3377b61557b318a567b469046ad9
d-otis <dan@dan-foley.com>
2020-10-16 21:29:09 -0400

    add reviews model/migration

---
+++ db/migrate/20201016212422_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews, id: :uuid do |t|
+      t.text :content
+      t.uuid :property_id
+      t.float :rating
+
+      t.timestamps
+    end
+  end
+end
