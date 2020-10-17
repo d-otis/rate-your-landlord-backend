6657798e384dc2d0d9f028bb8bf960ab809af952
d-otis <dan@dan-foley.com>
2020-10-17 10:58:50 -0400

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Landlord.destroy_all
+Property.destroy_all
+
+num_landlords = 5
+
+num_landlords.times do 
+  Landlord.create(
+    name: Faker::Name.name,
+    rating: rand * 5
+  )
+end
+
+puts "You have made #{Landlord.count} landlords"
+
+5.times do 
+  Landlord.all.each do |ll|
+    property = Property.create(
+      address: Faker::Address.full_address,
+      rating: rand * 5
+    )
+    ll.properties << property
+  end
+end
+
+puts "#{Property.count} properties created!"
+
+# Make Reviews!
