be6717ef92d48f6410c7cc7ddcb5487c52882a80
d-otis <dan@dan-foley.com>
2020-10-17 17:16:01 -0400

    define landlords#index action

--- app/controllers/api/v1/landlords_controller.rb
+++ app/controllers/api/v1/landlords_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::LandlordsController < ApplicationController
+  def index
+    landlords = Landlord.order(created_at: :desc)
+
+    render json: LandlordSerializer.new(landlords).serialized_json
+  end
+
+  private
+
+# add in once properties serializer is fleshed out
+  # def options
+  #   options = {
+  #     include: [:properties]
+  #   }
+  # end
 end
