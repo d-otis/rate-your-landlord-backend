fd5b50031087d2a0c101c2b1aaf4f00885c7ddca
d-otis <dan@dan-foley.com>
2020-10-19 11:12:51 -0400

    add class instance methods for Landlord rating information based off personal and property averages and aggregate avg

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,4 +1,18 @@
 class Landlord < ApplicationRecord
   has_many :reviews, as: :reviewable
   has_many :properties
+
+  def aggregate_rating
+    (personal_review_avg + properties_review_avg) / 2
+  end
+
+  def personal_review_avg
+    self.reviews.collect {|review| review.rating}.sum / self.reviews.count
+  end
+
+  def properties_review_avg
+    properties_review_avg = self.properties.collect do |property|
+      property.reviews.collect {|review| review.rating}.sum / property.reviews.count
+    end.sum
+  end
 end
