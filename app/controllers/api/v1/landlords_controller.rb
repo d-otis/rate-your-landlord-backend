9634d403ab993015e5d8760b9ca1814ab3c45bb1
d-otis <dan@dan-foley.com>
2020-10-19 15:04:53 -0400

    add landlords#show action and route for development purposes only

--- app/controllers/api/v1/landlords_controller.rb
+++ app/controllers/api/v1/landlords_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::LandlordsController < ApplicationController
+
   def index
     landlords = Landlord.order(created_at: :desc)
 
     render json: LandlordSerializer.new(landlords, options).serialized_json
+  end
+
+  def show
+    landlord = Landlord.find_by(id: params[:id])
+
+    render json: LandlordSerializer.new(landlord, options).serialized_json
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      resources :landlords, only: [:index]
+      resources :landlords, only: [:index, :show]
       resources :properties, only: [:index]
     end
   end
