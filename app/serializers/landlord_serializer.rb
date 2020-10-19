9fa85ffe156c0cc3f80f3b8d6d682f6921d14251
d-otis <dan@dan-foley.com>
2020-10-19 14:50:49 -0400

    add custome attribute to LandlordSerializer

--- app/serializers/landlord_serializer.rb
+++ app/serializers/landlord_serializer.rb
@@ -2,4 +2,9 @@ class LandlordSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :created_at
   has_many :properties
+  has_many :reviews
+
+  attribute :aggregate_rating do |ll|
+    ll.property_review_avg
+  end
 end
