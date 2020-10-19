be5e4957e7570788a0e500cb26e30e19763947de
d-otis <dan@dan-foley.com>
2020-10-19 11:11:19 -0400

    remove landlord id from review migration

--- db/migrate/20201016212422_create_reviews.rb
+++ db/migrate/20201016212422_create_reviews.rb
@@ -2,7 +2,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews, id: :uuid do |t|
       t.text :content
-      t.uuid :property_id
       t.integer :rating
 
       t.timestamps
