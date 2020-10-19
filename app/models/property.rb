eb614a10a7d4c4630a245acbbee1bbf8b00ea9b6
d-otis <dan@dan-foley.com>
2020-10-19 18:23:42 -0400

    add image attachment to property class

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,7 @@
 class Property < ApplicationRecord
   has_many :reviews, as: :reviewable
   belongs_to :landlord
+  has_one_attached :image
 
   def review_average
     (self.reviews.collect {|review| review.rating}.flatten.sum / self.reviews.count.to_f).round(2)
