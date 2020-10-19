c0f421b5ef4b4b5794496c180c5536a7478f01dd
d-otis <dan@dan-foley.com>
2020-10-19 15:31:56 -0400

    add custome instance method to properties for review average

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
   has_many :reviews, as: :reviewable
   belongs_to :landlord
+
+  def review_average
+    (self.reviews.collect {|review| review.rating}.flatten.sum / self.reviews.count.to_f).round(2)
+  end
+
 end
