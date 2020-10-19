d44e51db9043d7792ceb096ddbff5d4d2bcef065
d-otis <dan@dan-foley.com>
2020-10-19 11:27:49 -0400

    convert rating return to float and round to nearest hundredth

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -3,16 +3,16 @@ class Landlord < ApplicationRecord
   has_many :properties
 
   def aggregate_rating
-    (personal_review_avg + properties_review_avg) / 2
+    ((personal_review_avg + properties_review_avg) / 2.0).round(2)
   end
 
   def personal_review_avg
-    self.reviews.collect {|review| review.rating}.sum / self.reviews.count
+    (self.reviews.collect {|review| review.rating}.sum / self.reviews.count.to_f).round(2)
   end
 
-  def properties_review_avg
+  def property_review_avg
     properties_review_avg = self.properties.collect do |property|
-      property.reviews.collect {|review| review.rating}.sum / property.reviews.count
+      (property.reviews.collect {|review| review.rating}.sum / property.reviews.count.to_f).round(2)
     end.sum
   end
 end
