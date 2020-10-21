1050e31e1e3076cd0d72a997e63af7d4ede828a9
d-otis <dan@dan-foley.com>
2020-10-21 10:54:14 -0400

    add property image url to serialized JSON object

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,10 +1,19 @@
 class PropertySerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :address, :landlord_id, :image
+  attributes :address, :landlord_id
   has_many :reviews
 
   attribute :review_average do |property|
     property.review_average
   end
+
+  attribute :has_image do |property|
+    property.image.attached?
+  end
+
+  attribute :image_url do |property|
+    Rails.application.routes.url_helpers.rails_blob_url(property.image, host: 'localhost:3000')
+  end
+
 end
