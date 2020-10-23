f7cb960ebce4f28736c580b7b85fcc8cfaaf5995
d-otis <dan@dan-foley.com>
2020-10-23 15:31:58 -0400

    update serializers to changes made in model associations

--- app/serializers/landlord_serializer.rb
+++ app/serializers/landlord_serializer.rb
@@ -1,10 +1,7 @@
 class LandlordSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :created_at
+  attributes :name, :created_at, :id
   has_many :properties
   has_many :reviews
 
-  attribute :aggregate_rating do |ll|
-    ll.property_review_avg
-  end
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,4 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :content, :rating
+  attributes :content, :rating, :property_id
 end
