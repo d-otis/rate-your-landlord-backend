d9fbd92fd9db2571a68406d9720ec5589956c6bf
d-otis <dan@dan-foley.com>
2020-10-24 17:47:55 -0400

    remove obsolete Landlord instance methods

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,20 +1,4 @@
 class Landlord < ApplicationRecord
   has_many :properties, :dependent => :destroy
   has_many :reviews, :through => :properties
-
-  def aggregate_rating
-    ((personal_review_avg + property_review_avg) / 2.0).round(2)
-  end
-
-  def personal_review_avg
-    (self.reviews.collect {|review| review.rating}.sum / self.reviews.count.to_f).round(2)
-  end
-
-  def property_review_avg
-    ratings_arr = self.properties.collect do |p|
-      p.reviews.collect {|r| r.rating}
-    end.flatten
-
-    (ratings_arr.sum / ratings_arr.count.to_f).round(2)
-  end
 end
