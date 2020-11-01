845c5993bbb9e5027e0215ce3c09f74988694093
d-otis <dan@dan-foley.com>
2020-11-01 12:24:49 -0500

    stub out reviews#create action in ReviewsController

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -6,4 +6,17 @@ class Api::V1::ReviewsController < ApplicationController
     render json: ReviewSerializer.new(reviews).serialized_json
   end
 
+  def create
+    binding.pry
+    # make sure to trigger rating update 
+    # after this review is created 
+    # for landlord AND property
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(:content, :rating, :property_id)
+  end
+
 end
