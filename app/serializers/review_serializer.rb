bc04159e71cd5205983fb61b1ee7be0de8430374
d-otis <dan@dan-foley.com>
2020-11-03 16:23:11 -0500

    add custom attributes to Reviews JSON to aid in updating shared data across resources

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,16 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
   attributes :content, :rating, :property_id
+
+  attribute :landlord_id do |review|
+    review.property.landlord_id
+  end
+
+  attribute :landlord_rating do |review|
+    review.property.landlord.rating
+  end
+
+  attribute :property_rating do |review|
+    review.property.rating
+  end
 end
