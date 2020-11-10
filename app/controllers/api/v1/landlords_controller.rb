096e048384bcef37a48a87114c969fa1c38efb0d
d-otis <dan@dan-foley.com>
2020-11-09 20:27:12 -0500

    update landlord_params to include :image_url

--- app/controllers/api/v1/landlords_controller.rb
+++ app/controllers/api/v1/landlords_controller.rb
@@ -50,7 +50,7 @@ class Api::V1::LandlordsController < ApplicationController
   end
 
   def landlord_params
-    params.require(:landlord).permit(:name, properties_attributes: [:address])
+    params.require(:landlord).permit(:name, properties_attributes: [:address, :image_url])
   end
 
   def set_landlord
