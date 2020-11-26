08c29deaa0980c6730a30791a33312138a563082
d-otis <dan@dan-foley.com>
2020-11-26 17:56:55 -0500

    add image_url to landlord serializer

--- app/serializers/landlord_serializer.rb
+++ app/serializers/landlord_serializer.rb
@@ -1,6 +1,6 @@
 class LandlordSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :created_at, :id, :rating
+  attributes :name, :created_at, :id, :rating, :image_url
   has_many :properties
   has_many :reviews
 
