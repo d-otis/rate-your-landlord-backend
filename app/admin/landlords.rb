7d24c4800032b17ac38b70012b6ed2c465671384
d-otis <dan@dan-foley.com>
2020-11-26 14:27:11 -0500

    customize landlord admin page

--- app/admin/landlords.rb
+++ app/admin/landlords.rb
@@ -3,9 +3,13 @@ ActiveAdmin.register Landlord do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
+
+  scope :all, default: true
+  filter :name
+  filter :rating
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :rating
+  permit_params :name, :rating, :id
   #
   # or
   #
